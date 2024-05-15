8b80cccc34b651a7a41dc51046c39cf954354b81
Enes Büyük <contact@enesbuyuk.com>
2024-05-16 00:42:21 +0300

    edited main

--- main.py
+++ main.py
@@ -16,25 +16,6 @@ from modules.kutuphane import Kutuphane
 """
 
 
-
-
-def kitap_detay(kitap_id, kitap_liste):
-    for index, detay in kitap_liste.items():
-        if kitap_id == index:
-            odunc_mesaj = "Bu kitabı ödünç alabilirsiniz."
-            if detay["odunc"] == 0:
-                odunc_mesaj = """Bu kitap şu an başkası tarafından ödünç alınmıştır.\n
-            	Tahmini kalan teslim süresi: {detay["kalanSure"]} gün"""
-            print(f"""Kitap benzersiz ID: {detay["id"]}
-            Kitap adı: {detay["isim"]}
-       	    Kitap basım yılı: {detay["yil"]}
-       	    Kitap yazarı: {detay["yazar"]}
-       		{odunc_mesaj}
-        	""")
-            break
-
-
-
 def main():
     OBJ_kutuphane = Kutuphane(veritabani)
     OBJ_kutuphane.ana_ekran()
